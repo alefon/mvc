2de43899ab87562440da472e85284496cc7f75ef
José Alejandro Fonseca Segura <alefon51s@gmail.com>
2017-10-18 02:40:29 -0600

    Initial commit

---
+++ ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace mvc
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        //Este lo vamos para ver que los usuarios estén conectados
+        //public override System.Threading.Tasks.Task OnConnected()
+        //{
+        //    Clients.All.user(Context.User.Identity.Name);
+        //    return base.OnConnected();
+        //}
+
+        public void send(string name , string message)
+        {
+            Clients.Caller.message("Yo: " + message);
+            Clients.Others.message(name + " : " + message);
+        }
+
+    }
+}
